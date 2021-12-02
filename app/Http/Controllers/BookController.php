50b8d03055d77cb5e211d73995abd2f150b61305
indriwld <indri_240@smkassalaambandung.sch.id>
2021-12-02 13:24:25 +0700

    crud

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,125 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $book = Book::all();
+        return view('book.index', compact('book'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $author = Author::all();
+        return view('book.create', compact('author'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = new Book;
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        // upload image / foto
+        if ($request->hasFile('cover')) {
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('image/book/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+        return redirect()->route('book.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $book = Book::findOrFail($id);
+        $author = Author::all();
+        return view('book.edit', compact('book', 'author'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        // upload image / foto
+        if ($request->hasFile('cover')) {
+            $book->deleteImage();
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('image/book/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+
+        return redirect()->route('book.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->deleteImage();
+        $book->delete();
+
+        return redirect()->route('book.index');
+    }
+}
